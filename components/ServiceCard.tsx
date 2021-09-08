bd390f4e72563aa8fd73f1b79a60f506996f136b
Shreyash Chooudhary <37010919+shreyashji@users.noreply.github.com>
2021-09-08 16:12:34 +0530

    updating servicecard

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -16,7 +16,7 @@ const createMarkup = () => {
       <div className='flex items-center p-2 space-x-4 '>
          <Icon className='w-12 h-12 text-green' />
          <div>
-            <h4 className='font-bold'>{title}</h4>
+            <h5 className='font-bold'>{title}</h5>
             <p dangerouslySetInnerHTML={createMarkup()} />
          </div>
       </div>
