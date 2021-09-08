7ac4b6dc3bab0e63761d7d07dadbe7526d5e4273
Shreyash Chooudhary <37010919+shreyashji@users.noreply.github.com>
2021-09-08 16:08:51 +0530

    updating document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,15 @@ class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" 
             rel="stylesheet"/>
+            <meta 
+            name="description"
+            content="Shreyash Choudhary | Software Devloper"
+            />
+            <meta 
+            name="keywords"
+            content="Software Devloper, Software Engineer, SDE, SWE, Data Engineer"
+            />
+
             </Head>
             <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-dark-500 dark:to-dark-700 dark:text-white">
                <Main />
